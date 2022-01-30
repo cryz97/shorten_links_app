c6c53dd82183c7572b42e8df0cd2dd3ca443d62a
cryz97 <paladincryz@gmail.com>
2022-01-30 15:39:59 -0600

    refactor home page

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shorten_links_app/bloc/shorten_url_bloc.dart';
+import 'package:shorten_links_app/ui/widgets/input_url_row.dart';
+import 'package:shorten_links_app/ui/widgets/list_view_url.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final urlBloc = ShortenUrlBloc();
     return Scaffold(
         appBar: AppBar(
           title: const Text("Shorten Links"),
         ),
-        body: StreamBuilder(
-            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          return ListView.builder(itemBuilder: (_, i) {
-            return ListTile(title: Text(i.toString()));
-          });
-        }));
+        body: Column(
+          children: [
+            InputRow(
+              urlBloc: urlBloc,
+            ),
+            ListViewUrl(
+              urlBloc: urlBloc,
+            )
+          ],
+        ));
   }
 }
